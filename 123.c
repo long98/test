5e825cefbb59ffcec7f896fb791727985387364d
long98 <w137350201@outlook.com>
2025-10-24 16:41:33 +0800

    test

---
+++ 123.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> // 引入时间库，用于获取当前时间
+		  //
+		  // int main() {
+		  //     // 初始化随机数种子
+		  //         srand((unsigned)time(NULL)); // 使用当前时间作为种子
+		  //
+		  //             // 生成一个0到99之间的随机数
+		  //                 int randomNumber = rand() % 100;
+		  //
+		  //                     printf("生成的随机数是: %d\n", randomNumber);
+		  //
+		  //                         return 0;
+		  //                         }
+		  //
